39883344aa51a14cc11946bce63a95cdda8f7b61
ankit1350 <171828455+ankit1350@users.noreply.github.com>
2025-04-23 19:57:12 +0530

    my love

---
+++ 230425.py
@@ -0,0 +1,8 @@
+print('''what i feel for you is so much more than i can say
+      no one compares to what you are
+      i know i knew it from the start
+      promise me my love, don't take another heart
+      i've been hurt before i wanna new start,
+      no i know  you are not my first love,
+      but i know that from today,
+      we can say history , stays history and you will always love me''')
