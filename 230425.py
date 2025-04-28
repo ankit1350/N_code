bf298c5772c26e1200f740a15dbf908a6e948454
ankit1350 <171828455+ankit1350@users.noreply.github.com>
2025-04-28 18:50:49 +0530

    new edited

--- 230425.py
+++ 230425.py
@@ -1,6 +1,6 @@
-print('''what i feel for you is so much more than i can say
+print('''what i feel with you is so much more than i can tell
       no one compares to what you are
-      i know i knew it from the start
+      i know i knew it from the start.
       promise me my love, don't take another heart
       i've been hurt before i wanna new start,
       no i know  you are not my first love,
